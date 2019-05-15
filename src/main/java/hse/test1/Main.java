3ffaf7d219250bed41c56176b9208c8197f27080
Gavrilov <gavrilov_dan@mail.ru>
2019-05-15 17:09:28 +0300

    build fixed

--- src/main/java/hse/test1/Main.java
+++ src/main/java/hse/test1/Main.java
@@ -2,14 +2,15 @@ package hse.test1;
 
 public class Main {
     public static void main(String[] args) {
-        if (args.length < 2) {
+        if (args.length < 1) {
             System.out.println("Type file path");
+            return;
         }
         var time = System.currentTimeMillis();
-        var resultOne = CheckSumComputerOneThread.compute(args[1]);
+        var resultOne = CheckSumComputerOneThread.compute(args[0]);
         var elapsedTimeOne = System.currentTimeMillis() - time;
         time = System.currentTimeMillis();
-        var resultMany = CheckSumComputerOneThread.compute(args[1]);
+        var resultMany = CheckSumComputerOneThread.compute(args[0]);
         var elapsedTimeMany = System.currentTimeMillis() - time;
         if (resultOne != null) {
             System.out.println("One thread result: " + resultOne);
